3c6603ceb49d456427bea7a7bce8a056ce9bba69
Nathan Perfetti <nathanperfetti@gmail.com>
2023-02-04 10:44:57 -0600

    added env setup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+require("dotenv").config()
+
+console.log("+++ Connecting to MONGODB\n", process.env.MONGODB_URI)
+console.log("\n+++ Connecting to ENV\n", process.env)
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts',
